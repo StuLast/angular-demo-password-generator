2c9d794a31262539091b2748208667ac5dbe7cea
Stu Last <stuart@theaudioprogrammer.com>
2021-11-23 16:53:12 +0000

    Wires up 'onClick' of generate button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'part2-password-generator';
+  password = '';
+  passwordLength = 0;
+  useLetters = true;
+  useNumbers =  false;
+  useSymbols = false;
+
+  private generatePassword = ():string => {
+    console.log(this.passwordLength)
+    let newPassword = '';
+
+    for(let i = 0; i < this.passwordLength; i++) {
+      console.log(this.generateCharacter());
+      newPassword = newPassword + this.generateCharacter();
+    }
+
+    return newPassword;
+  }
+
+  private generateCharacter = (): string => {
+    
+    return "a";
+  }
+
+  onButtonClick = ():void =>  {
+    this.password = this.generatePassword();
+    console.log(this.password)
+  }
 }
